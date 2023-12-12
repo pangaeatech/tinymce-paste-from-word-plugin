bd23ee9f4e6bbf428d974faeaa1dccad5112bdfb
Michael Waddell <michael@waddellnet.com>
2023-12-12 16:00:18 -0600

    Using webpack instead of ncc (#19)

---
+++ webpack-lib.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/plugin/lib.ts",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: "index.js",
+    globalObject: "this",
+    clean: true,
+    library: {
+      name: "tinymce-paste-from-word-lib",
+      type: "umd",
+    },
+  },
+  externals: {
+    tinymce: {
+      commonjs: "tinymce",
+      commonjs2: "tinymce",
+      amd: "tinymce",
+      root: "tinymce",
+    },
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.ts",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    globalObject: "this",
+    clean: true,
+    library: {
+      name: "tinymce-paste-from-word-plugin",
+      type: "umd",
+    },
+  },
+  externals: {
+    tinymce: {
+      commonjs: "tinymce",
+      commonjs2: "tinymce",
+      amd: "tinymce",
+      root: "tinymce",
+    },
+  },
+};
